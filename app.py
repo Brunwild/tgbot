1178ae32fd8e677f7cbd749af279d6a6f0cb077c
Stanislav Chudinovskikh <s.a.chudinovskikh@gmail.com>
2024-04-25 13:51:06 +0300

    v. 2

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import asyncio
 import os
-from venv import create
 
 from aiogram import Bot, Dispatcher, types
 from aiogram.enums import ParseMode
@@ -11,23 +10,21 @@ load_dotenv(find_dotenv())
 
 from middlewares.db import DataBaseSession
 
-
 from database.engine import create_db, drop_db, session_maker
 
 from handlers.user_private import user_private_router
 from handlers.user_group import user_group_router
 from handlers.admin_private import admin_router
 
-from common.bot_cmds_list import private
+# from common.bot_cmds_list import private
 
 
-#ALLOWED_UPDATES = ['message, edited_message', 'callback_query']
+# ALLOWED_UPDATES = ['message', 'edited_message', 'callback_query']
 
 bot = Bot(token=os.getenv('TOKEN'), parse_mode=ParseMode.HTML)
 bot.my_admins_list = []
 
 dp = Dispatcher()
-
 
 dp.include_router(user_private_router)
 dp.include_router(user_group_router)
@@ -36,24 +33,24 @@ dp.include_router(admin_router)
 
 async def on_startup(bot):
 
-    run_param = False
-    if run_param:
-        await drop_db()
+    # await drop_db()  #снос базы данных
 
     await create_db()
 
 
 async def on_shutdown(bot):
-    print('бот лёг')
+    print('бот лег')
+
 
 async def main():
     dp.startup.register(on_startup)
     dp.shutdown.register(on_shutdown)
 
     dp.update.middleware(DataBaseSession(session_pool=session_maker))
-    
+
     await bot.delete_webhook(drop_pending_updates=True)
-    await bot.set_my_commands(commands=private, scope=types.BotCommandScopeAllPrivateChats())
+    # await bot.delete_my_commands(scope=types.BotCommandScopeAllPrivateChats())   #убрать кнопку меню
+    # await bot.set_my_commands(commands=private, scope=types.BotCommandScopeAllPrivateChats())
     await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
 
 asyncio.run(main())
